88ee68a7533d46dc90f48b20e1bdf0d09d4ca916
Artur Barsegyan <a.barsegyan@corp.mail.ru>
2019-01-07 18:35:42 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+(function () {
+	chrome.runtime.onInstalled.addListener(function() {
+		chrome.contextMenus.create({
+	      "id": "sampleContextMenu",
+	      "title": "Sample Context Menu",
+	      "contexts": ["selection"]
+	    });
+	})
+})
