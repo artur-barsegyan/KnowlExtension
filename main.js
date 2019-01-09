d4d933650b41f29cd58de734eea2661c7bfc8543
Artur Barsegyan <a.barsegyan@corp.mail.ru>
2019-01-09 18:40:17 +0300

    find a way to load css externally

--- main.js
+++ main.js
@@ -1,29 +1,75 @@
 (function () {
 	var curWindow = this; // class Window
-	var isActive = false;
-	// var userAgent = "";  
+	var isOpened = false;
+	// var userAgent = "";
  //    if (curWindow.navigator) {
  //        if (curWindow.navigator.userAgent) {
  //            userAgent = curWindow.navigator.userAgent;
  //        }
  //    }
 
-	document.addEventListener("mouseup", function(event) { 
+	document.addEventListener("mouseup", function(event) {
 		var selection = curWindow.getSelection();
 		var selectedText = selection.toString().trim();
 
-		if (selectedText != "") {
+		if (selectedText != "" && !isOpened) {
 			addPopupIcon(event, selection);
-			console.log(selectedText); 
+			console.log(selectedText);
 		}
 	});
 
-	document.addEventListener("mousedown", function(event) {
+	var removeKDBDiv = function() {
 		var mainBlock = document.getElementById("kdb");
 		if (mainBlock != null) {
 			document.body.removeChild(mainBlock);
 		}
+	};
+
+	document.addEventListener("mousedown", function(event) {
+		if (isOpened) {
+			return;
+		}
+
+		var mainBlock = document.getElementById("kdb");
+		if (mainBlock != null) {
+			// Click on popup icon
+			if (event.target == document.getElementsByClassName("kdb-popup-icon")[0]) {
+				mainBlock.removeChild(document.getElementsByClassName("kdb-popup-icon")[0]);
+				clickEventHandler(mainBlock, event);
+			} else {
+				document.body.removeChild(mainBlock);
+			}
+		}
 	});
+
+	var clickEventHandler = function(mainBlock, clickEvent) {
+		fetch(chrome.runtime.getURL('/popup_window.html'))
+    	.then(response => response.text())
+    	.then(data => {
+			var closeImgURL = chrome.runtime.getURL("images/close.png");
+			var cssURL = chrome.runtime.getURL("popup.css");
+
+			document.head.innerHTML += "<link rel=\"stylesheet\" href=\"" + cssURL + "\">";
+			mainBlock.innerHTML = data;
+			document.getElementById("closeButtonIcon").src = closeImgURL;
+
+			document.getElementById("closeButton").onclick = function() {
+				isOpened = false;
+				removeKDBDiv();
+			};
+
+			// console.log("HERE232323");
+			mainBlock.style.position = "absolute";
+			mainBlock.style.left = clickEvent.clientX - 13 + "px";
+			var yPos = clickEvent.clientY;
+			// yPos - selection.top > selection.height / 2 ? yPos = selection.bottom + 1 : yPos = selection.top - 1 - 27;
+			mainBlock.style.top = yPos + "px";
+
+			isOpened = true;
+	    }).catch(err => {
+	        console.log("Error! Can't inject popup window in html. Reason: " + err);
+	    });
+	};
 
     // Only for Chrome
 	var someScrollingCheck = function(doc) {
@@ -35,6 +81,8 @@
 
 		var iconBlock = document.createElement("div");
 		iconBlock.className = "kdb-popup-icon";
+		iconBlock.onclick = clickEventHandler;
+
 		var mainBlock = document.createElement("div");
 		mainBlock.appendChild(iconBlock);
 
